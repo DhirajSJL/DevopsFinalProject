e4c7b918be9dcf4b455873d0b3824b367b708f1b
Dhirajkumar <dhiraj6.lulla@rediffmail.com>
2025-05-10 14:33:17 +0530

    Output Message Changed in .java file

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
     public String hello() {
         //return "Hello from Spring Boot + PostgreSQL!"
         //return "Hello from Dhirajkumar Lulla + DB tech!";
-        return "Hello from Project which executes Docker with Jenkins ";
+        return "Hello from Java Project which is executed with Jenkins, Docker and Kubernetes ";
     }
 }
